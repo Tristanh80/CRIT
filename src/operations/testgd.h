ad8f1172d97662d37be6e15faf9ccdb22e225a50
Romain Busani <romain.busani@epita.fr>
2021-05-03 23:12:45 +0200

    testgd.h

--- src/operations/testgd.h
+++ src/operations/testgd.h
@@ -8,7 +8,7 @@ void negate(gdImagePtr img, FILE *fdout, char *path);
 void emboss(gdImagePtr img, FILE *fdout, char *path);
 void contrast(gdImagePtr img, FILE *fdout, char *path, int val);
 void crop(gdImagePtr img, FILE *fdout, char *path, int a, int b);
-void sepia(gdImagePtr img, FILE *fdout, char *path);
+//void sepia(gdImagePtr img, FILE *fdout, char *path);
 
 
 #endif
