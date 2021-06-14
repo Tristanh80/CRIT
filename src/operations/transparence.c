47ada54d1098b4fe90fa3c76c1f85f403a154854
Iony Rakotoson <rakotoson.iony@gmail.com>
2021-06-14 20:36:31 +0200

    last test

--- src/operations/transparence.c
+++ src/operations/transparence.c
@@ -1,4 +1,5 @@
 #include "../import/import.h"
+#include "grey.h"
 #include <stdlib.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
@@ -8,6 +9,7 @@
 void transparence (SDL_Surface *img, int alpha)
 {
   SDL_Surface *ecran = img;
+  greyscale(img);
   SDL_Surface *filter = img;
 
   SDL_Rect position;
@@ -15,8 +17,7 @@ void transparence (SDL_Surface *img, int alpha)
   position.y = 0;
   
   SDL_SetColorKey(filter, SDL_SRCCOLORKEY, SDL_MapRGB(filter->format, 0, 0, 0));
-  //SDL_SetAlpha(img, SDL_SRCALPHA, 255);
   SDL_SetAlpha(filter, SDL_SRCALPHA, alpha);
-  SDL_BlitSurface(filter, NULL, img,  &position);
+  SDL_BlitSurface(filter, NULL, ecran,  &position);
   
 }
