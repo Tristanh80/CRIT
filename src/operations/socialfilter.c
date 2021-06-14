9b582e8bf2af4fe6468092bf0275e13bc039d40c
Tristan HERSENT <tristan.hersent@epita.fr>
2021-06-14 12:14:10 +0200

    filters update

--- src/operations/socialfilter.c
+++ src/operations/socialfilter.c
@@ -5,32 +5,33 @@
 #include <err.h>
 #include <stdio.h>
 
-void filter(SDL_Surface *img, int choice, int alpha)
+void filters(SDL_Surface *img, int choice, int alpha)
 {
-  SDL_Surface *ecran = img;
-  SDL_Surface *filter;
-  
-  if (choice == 1)
+    SDL_Surface *filter = load_image("filter1.png");
+
+    if (choice == 1)
     {
       filter = load_image("filter1.png");
     }
 
-  if (choice == 2)
+    if (choice == 2)
     {
       filter = load_image("filter2.png");
     }
 
-  if (choice == 3)
+    if (choice == 3)
     {
       filter = load_image("filter3.png");
     }
+    
+    
 
-  SDL_Rect position;
-  position.x = 0;
-  position.y = 0;
-  
-  SDL_SetColorKey(filter, SDL_SRCCOLORKEY, SDL_MapRGB(filter->format, 0, 0, 0));
-  SDL_SetAlpha(filter, SDL_SRCALPHA, alpha);
-  SDL_BlitSurface(filter, NULL, img,  &position);
+    SDL_Rect position;
+    position.x = 0;
+    position.y = 0;
+
+    SDL_SetColorKey(filter, SDL_SRCCOLORKEY, SDL_MapRGB(filter->format, 0, 0, 0));
+    SDL_SetAlpha(filter, SDL_SRCALPHA, alpha);
+    SDL_BlitSurface(filter, NULL, img,  &position);
   
 }
--- src/operations/socialfilter.h
+++ src/operations/socialfilter.h
@@ -1,10 +1,10 @@
-#ifndef TRANSPARENCE_H
-#define TRANSPARENCE_H
+#ifndef SOCIALFILTER_H
+#define SOCIALFILTER_H
 
 #include <SDL/SDL_image.h>
 #include <SDL/SDL.h>
 #include "../import/import.h"
 
-void filter(SDL_Surface *img, int choice, int alpha);
+void filters(SDL_Surface *img, int choice, int alpha);
 
 #endif
